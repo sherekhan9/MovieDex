070678f49feaf5903ef76210ba122b229bb36a70
sherekhan9 <sherekhan9@gmail.com>
2023-02-28 16:09:56 -0600

    Ver1

--- MovieDexV7/app/src/main/java/com/example/moviedexv7/MainActivity.java
+++ MovieDexV7/app/src/main/java/com/example/moviedexv7/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.moviedexv7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
 
     @Override
@@ -15,11 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homemenu);
 
+
         configureADDButton();
         configureSEARCHButton();
     }
 
-    private void configureADDButton(){
+    private void configureADDButton() {
 
         Button addMovieButton = (Button) findViewById(R.id.GoToAdd);
         addMovieButton.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startAddMovieActivity();
             }
+
             private void startAddMovieActivity() {
                 Intent intent = new Intent(MainActivity.this, AddActivity.class);
                 startActivity(intent);
@@ -34,21 +39,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    private void configureSEARCHButton(){
-            Button searchMovieButton = (Button) findViewById(R.id.GoToSearch);
-            searchMovieButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this,SearchActivity.class);
-                    startActivity(intent);
-                }
-            });
 
+    private void configureSEARCHButton() {
+        Button searchMovieButton = (Button) findViewById(R.id.GoToSearch);
 
-        };
-
-
+        searchMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SearchActivityMain.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    ;
+}
 
 
 
