9c12f94a68a436310e253f617c793d3db3e0b24f
John <meep2516@pop-os.localdomain>
2023-02-27 22:59:46 -0600

    ver1

--- MovieDexV7/app/src/main/java/com/example/moviedexv7/MovieAdapter.java
+++ MovieDexV7/app/src/main/java/com/example/moviedexv7/MovieAdapter.java
@@ -1,13 +1,52 @@
 package com.example.moviedexv7;
 
-import android.widget.Filter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+private String[] localdataCHANGE;
 
-public class MovieAdapter{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+            // COULD PUT A ON CLICK LISTENER IN HERE(:
+            textView = (TextView) view.findViewById(R.id.textView);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
 
 
+    public MovieAdapter(String[] dataSet) {
+        localdataCHANGE = dataSet;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.activity_search, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localdataCHANGE[position]);
+    }
+    @Override
+    public int getItemCount() {
+        return localdataCHANGE.length;
+    }
 }
+
